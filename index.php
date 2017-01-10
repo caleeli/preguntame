a123a3833e8e4d0e0dfc9a36f2dbf7221017e255
davidcallizaya <davidcallizaya@192.168.11.22>
2017-01-10 17:50:20 -0400

    get images

--- index.php
+++ index.php
@@ -6,5 +6,17 @@
         <form>
             <input name="p" style="width: 100%">
         </form>
+        <?php
+        if(!empty($_GET['p'])) {
+            $dom = new DOMDocument();
+            $dom->loadHTMLFile('https://twitter.com/hashtag/'.preg_replace('/[^a-z0-9]+/i', '-', $_GET['p']));
+            foreach($dom->getElementsByTagName('img') as $img) {
+                if($img->hasAttribute('data-aria-label-part')) {
+                    $src = $img->getAttribute('src');
+                    echo '<img src="'.$src.'" style="width:30%">';
+                }
+            }
+        }
+        ?>
     </body>
 </html>
